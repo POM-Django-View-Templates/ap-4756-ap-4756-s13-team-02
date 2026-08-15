a6ac6551864b1235b127e57508684f602ccca521
Maksym Chumachenko <makchumachenko@gmail.com>
2026-08-15 14:37:50 +0300

    Added admin check to users

--- library/user/views.py
+++ library/user/views.py
@@ -1,14 +1,24 @@
 from django.shortcuts import render
 from django.contrib.auth import get_user_model
+from django.contrib.auth.decorators import user_passes_test
+
 
 User = get_user_model()
 
+
+def admin_check(user):
+    return user.is_superuser or user.is_staff
+
+
+@user_passes_test(admin_check)
 def user_list(request):
     users = User.objects.all()
     return render(request, "user/user_list.html", {
         "users": users
     })
 
+
+@user_passes_test(admin_check)
 def user_info(request, id: int):
     user = User.objects.get(pk=id)
     return render(request, "user/user.html", {"user": user})
